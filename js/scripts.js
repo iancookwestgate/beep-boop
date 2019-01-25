dcfcae367a7dfa7374953815b304512749b78a35
Ian Cook Westgate <iwestgate@hotmail.com>
2019-01-25 09:05:02 -0800

    Messaged away the 2 & 3 if statements to focus on making 1 work.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 var userNum = function(number) {
-  if(parseInt(number)%3 === 0) {
-    return "I'm sorry, Dave. I'm afraid I can't do that.";
-  }
-
-  if(parseInt(number)%2 === 0) {
-    return "Boop!";
-  }
+  // if(parseInt(number)%3 === 0) {
+  //   return "I'm sorry, Dave. I'm afraid I can't do that.";
+  // }
+  //
+  // if(parseInt(number)%2 === 0) {
+  //   return "Boop!";
+  // }
 
   if(parseInt(number)%1 === 0) {
     return "Beep!";
