93d784663effce68fecf804344e47e3c0496d19d
Ian Cook Westgate <iwestgate@hotmail.com>
2019-01-25 09:42:51 -0800

    Return to earlier basic state with working if statements.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,15 @@
 var userNum = function(number) {
   var parsedNum = parseInt(number);
 
-  // if(parsedNum %3 === 0) {
-  //   return "I'm sorry, Dave. I'm afraid I can't do that.";
-  // }
-  //
-  // if(parsedNum %2 === 0) {
-  //   return "Boop!";
-  // }
-
-  for(var i = 0; i < parsedNum.length; i++) {
-    if(parsedNum[i] == 1) {
-      parsedNum[i] = "Beep!";
-    }
+  if(parsedNum %3 === 0) {
+    return "I'm sorry, Dave. I'm afraid I can't do that.";
   }
-
-  // if(parsedNum %1 === 0) {
-  //   return "Beep!";
-  // }
+  if(parsedNum %2 === 0) {
+    return "Boop!";
+  }
+  if(parsedNum %1 === 0) {
+    return "Beep!";
+  }
 }
 
 
