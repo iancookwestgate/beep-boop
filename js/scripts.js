19e53c87e51c3149ad025fee221c20c39f0037ee
Ian Cook Westgate <iwestgate@hotmail.com>
2019-01-25 10:10:33 -0800

    Replace if statements with if else functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,40 @@
-var userNum = function(number) {
-  var parsedNum = parseInt(number);
+var numRead = function(yourNum){
+  var newLife = "";
+  var numArray = yourNum.split("");
 
-  if(parsedNum %3 === 0) {
-    return "I'm sorry, Dave. I'm afraid I can't do that.";
-  }
-  if(parsedNum %2 === 0) {
-    return "Boop!";
-  }
-  if(parsedNum %1 === 0) {
-    return "Beep!";
-  }
-}
+  for (var i=0; i <numArray.length; i++){
+    if(numArray[i] == 3) {
+      numArray[i] = "I'm sorry, Dave. I'm afraid I can't do that.";
+    } else if(numArray[i] == 2){
+    numArray[i] = "Boop!";
+    } else if(numArray[i] == 1){
+      numArray[i] = "Beep!";
+    }
+      newLife+=numArray[i];
+    }
 
+  return newLife;
+};
 
-
-
-
-
-
-
-
+// var userNum = function(number) {
+//   var parsedNum = parseInt(number);
+//
+//   if(parsedNum %3 === 0) {
+//     return "I'm sorry, Dave. I'm afraid I can't do that.";
+//   }
+//   if(parsedNum %2 === 0) {
+//     return "Boop!";
+//   }
+//   if(parsedNum %1 === 0) {
+//     return "Beep!";
+//   }
+// }
 
 $(document).ready(function(){
   $("form#beepBoop").submit(function(event){
     var userInput = $("input#userInput").val();
 
-    $("#output").text(userNum(userInput));
+    $("#output").text(numRead(userInput));
 
     event.preventDefault();
   });
