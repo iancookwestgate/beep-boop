3d58861ed2abb25d9f2443faf30075b7b6a8f9e5
Ian Cook Westgate <iwestgate@hotmail.com>
2019-01-25 11:07:09 -0800

    Alter for loop to allow for more number manipulation.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ var numRead = function(yourNum){
   var newLife = "";
   var numArray = yourNum.split("");
 
-  for (var i=0; i < 10; i++){
+  for (var i=0; i < parseInt(numArray.length); i++){
     if (numArray[i] == 3) {
       numArray[i] = " I'm sorry, Dave. I'm afraid I can't do that. ";
     } else if (numArray[i] == 2) {
@@ -15,20 +15,6 @@ var numRead = function(yourNum){
 
   return newLife;
 };
-
-// var userNum = function(number) {
-//   var parsedNum = parseInt(number);
-//
-//   if(parsedNum %3 === 0) {
-//     return "I'm sorry, Dave. I'm afraid I can't do that.";
-//   }
-//   if(parsedNum %2 === 0) {
-//     return "Boop!";
-//   }
-//   if(parsedNum %1 === 0) {
-//     return "Beep!";
-//   }
-// }
 
 $(document).ready(function(){
   $("form#beepBoop").submit(function(event){
